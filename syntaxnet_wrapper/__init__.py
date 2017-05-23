f5dba1de28ba450c3e626b196fae9b0e66b394b4
Mailo Arsac <mailo.arsac@me.com>
2017-05-23 10:36:59 -0700

    Update __init__.py

--- syntaxnet_wrapper/__init__.py
+++ syntaxnet_wrapper/__init__.py
@@ -1,10 +1,10 @@
 import yaml
 import os.path as path
+import os.environ as env
+print os.environ['HOME']
 
-# Load config file and paths
-config_file = yaml.load(open(path.join(path.dirname(__file__), "../config.yml")))
-config_syntaxnet = config_file['syntaxnet']
-root_dir = config_syntaxnet['ROOT_DIR']
-parser_eval_path = config_syntaxnet['PARSER_EVAL']
-context_path = config_syntaxnet['CONTEXT']
-model_path = config_syntaxnet['MODEL']
+# Set configs and paths
+root_dir = env['SYNTAXNET_ROOT_DIR']
+parser_eval_path = env['SYNTAXNET_PARSER_EVAL']
+context_path = env['SYNTAXNET_CONTEXT']
+model_path = env['SYNTAXNET_MODEL']
