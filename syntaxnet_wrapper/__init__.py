254f28a668a88879b20aa2a7dcdbf8ec53f1f1fa
Mailo Arsac <mailo.arsac@me.com>
2017-05-23 10:44:58 -0700

    Update __init__.py

--- syntaxnet_wrapper/__init__.py
+++ syntaxnet_wrapper/__init__.py
@@ -1,9 +1,10 @@
 import yaml
+import os
 import os.path as path
-import os.environ as env
+
 
 # Set configs and paths
-root_dir = env['SYNTAXNET_ROOT_DIR']
-parser_eval_path = env['SYNTAXNET_PARSER_EVAL']
-context_path = env['SYNTAXNET_CONTEXT']
-model_path = env['SYNTAXNET_MODEL']
+root_dir = os.getenv('SYNTAXNET_ROOT_DIR')
+parser_eval_path = os.getenv('SYNTAXNET_PARSER_EVAL')
+context_path = os.getenv('SYNTAXNET_CONTEXT')
+model_path = os.getenv('SYNTAXNET_MODEL')
